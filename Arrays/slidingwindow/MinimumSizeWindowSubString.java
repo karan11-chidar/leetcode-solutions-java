db7b7e74178b3bed6cdd56e49f028f86fce7b1e5
karan Chidar <karanchidar449@gmail.com>
2025-12-07 13:39:09 +0530

    sliding window

--- Arrays/slidingwindow/MinimumSizeWindowSubString.java
+++ Arrays/slidingwindow/MinimumSizeWindowSubString.java
@@ -1,21 +1,18 @@
 package slidingwindow;
-
 import java.util.HashMap;
 import java.util.Map;
-
 public class MinimumSizeWindowSubString {
-    public String minWindow(String s, String t) {
+ public String minWindow(String s, String t) {
         // Edge Case
-        if (s.length() < t.length())
-            return "";
+        if(s.length()<t.length()) return "";
         int size = s.length();
-        // Storing to character frequency
-        Map<Character, Integer> bag = new HashMap<>();
+        // Storing to character frequency 
+        Map<Character,Integer> bag = new HashMap<>();
         // Convert to t string in a map
-        for (int i = 0; i < t.length(); i++) {
+        for(int i=0; i<t.length(); i++){
             char ch = t.charAt(i);
             // Store in map
-            bag.put(ch, bag.getOrDefault(ch, 0) + 1);
+            bag.put(ch,bag.getOrDefault(ch,0)+1);
         }
         // Length to a window intail value to long
         int minLength = Integer.MAX_VALUE;
@@ -27,32 +24,31 @@ public class MinimumSizeWindowSubString {
         int i = 0;
         int j = 0;
         // Traverse in Orignal String
-        while (j < size) {
+        while(j<size){
             char currentChar = s.charAt(j);
-            // check ki bag me currentchar hai
-            if (bag.containsKey(currentChar)) {
+            // check ki bag me currentchar hai 
+            if(bag.containsKey(currentChar)){
                 // Toh count ko update karo
-                bag.put(currentChar, bag.get(currentChar) - 1);
-                // check kiCurrent char ki value 0 ho gyi
-                if (bag.get(currentChar) == 0)
-                    elementBag--;
+                bag.put(currentChar,bag.get(currentChar)-1);
+                // check kiCurrent char ki value 0 ho gyi 
+                    if(bag.get(currentChar)==0) elementBag--;
             }
             // so requirment is full
-            while (elementBag == 0) {
+            while(elementBag==0){
                 // update window size
-                if ((j - i + 1) < minLength) {
-                    minLength = j - i + 1;
+                if((j-i+1)<minLength){
+                    minLength = j-i+1;
                     startHead = i;
                 }
                 // Minimize window size
                 // Shrink window
                 char exitChar = s.charAt(i);
-                // check ki joh element bag se nikal rahe hai woh apne kaam ka toh nahi hai
-                if (bag.containsKey(exitChar)) {
+                //check ki joh element bag se nikal rahe hai woh apne kaam ka toh nahi hai 
+                if(bag.containsKey(exitChar)){
                     // Agar hai toh usse ++ karo kyuki hame woh chaiye
-                    bag.put(exitChar, bag.get(exitChar) + 1);
-                    // Check ki kya hamra exit wala element zero se greter hai
-                    if (bag.get(exitChar) > 0) {
+                    bag.put(exitChar,bag.get(exitChar)+1);
+                    // Check ki kya hamra exit wala element zero se greter hai 
+                    if(bag.get(exitChar)>0){
                         // Toh bag me element counter ko badao
                         elementBag++;
                     }
@@ -62,9 +58,8 @@ public class MinimumSizeWindowSubString {
             }
             j++;
         }
-        return (minLength == Integer.MAX_VALUE) ? "" : s.substring(startHead, startHead + minLength);
+        return (minLength==Integer.MAX_VALUE)?"":s.substring(startHead,startHead+minLength);
     }
-
     public static void main(String[] args) {
         MinimumSizeWindowSubString msws = new MinimumSizeWindowSubString();
         String s = "ADOBECODEBANC";
